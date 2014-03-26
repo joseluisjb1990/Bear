8ca18c4d0c76ef3c3e18576c92b5a073aba20f7d
Gabriela Limonta <glimonta@gmail.com>
2014-03-26 10:36:05 -0430

    La tabla solo se imprime si no hay errores

--- src/bear_driver.cc
+++ src/bear_driver.cc
@@ -18,11 +18,12 @@ bear_driver::parse (const std::string &f)
   yy::bear_parser parser (*this);
   //parser.set_debug_level (trace_parsing);
   int res = parser.parse ();
+  if (0 == errores) {
     /*for (unsigned int i=0; i < AST->size(); ++i) {
       cout << AST->at(i)->to_string() << std::endl;
     }*/
     cout << tabla;
-
+  }
   scan_end();
   return res;
 }
@@ -31,21 +32,21 @@ void
 bear_driver::error (const yy::location& l, const std::string& m)
 {
   ++errores;
-  std::cerr << "Error entre " << l.begin.line << "." << l.begin.column << " y " << l.end.line << "." << l.end.column << ": " << m << std::endl;
+  std::cerr << "Error between " << l.begin.line << "." << l.begin.column << " and " << l.end.line << "." << l.end.column << ": " << m << std::endl;
 }
 
 void
 bear_driver::error (const yy::location& l1, const yy::location& l2, const std::string& m)
 {
   ++errores;
-  std::cerr << "Error entre " << l1.begin.line << "." << l1.begin.column << " y " << l2.end.line << "." << l2.end.column << ": " << m << std::endl;
+  std::cerr << "Error between " << l1.begin.line << "." << l1.begin.column << " and " << l2.end.line << "." << l2.end.column << ": " << m << std::endl;
 }
 
 void
 bear_driver::error (const unsigned int LI, const unsigned int CI, const unsigned int LF, unsigned int CF, const std::string& m)
 {
   ++errores;
-  std::cerr << "Error entre " << LI << "." << CI << " y " << LF << "." << CF << ": " << m << std::endl;
+  std::cerr << "Error between " << LI << "." << CI << " and " << LF << "." << CF << ": " << m << std::endl;
 }
 
 void
